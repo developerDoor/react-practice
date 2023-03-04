5abbcfa7094cfeaf553fbfdcd4bb031ea7ed0d15
Moon_JangHyeok <dothebestmoon1016@gmail.com>
2023-03-04 14:42:55 +0900

    styled components

--- shop/src/routes/Detail.js
+++ shop/src/routes/Detail.js
@@ -1,4 +1,22 @@
 import {useParams} from "react-router-dom";
+import styled from 'styled-components';
+
+let YellowBtn = styled.button`
+	background: ${ props => props.bg };
+	color: ${ props => props.bg == 'blue' ? 'white' : 'black'};
+	padding: 10px;
+`
+
+let NewBtn = styled.button(YellowBtn)`
+
+`
+
+let Box =styled.div`
+	background: gray;
+	padding: 20px;
+`
+
+
 
 function Detail (props) {
 	let {id} = useParams();
@@ -6,9 +24,12 @@ function Detail (props) {
 	let data = props.shoes.find((e) => e.id == id)
 	return (
 		<div className="container">
+			{/*<Box>*/}
+			<YellowBtn bg="blue">버튼</YellowBtn>
+			{/*</Box>*/}
 			<div className="row">
 				<div className="col-md-6">
-					<img src={'https://codingapple1.github.io/shop/shoes'+ (Number(data.id)) +'.jpg'} width="100%" />
+					<img src={'https://codingapple1.github.io/shop/shoes'+ (Number(data.id)+1) +'.jpg'} width="100%" />
 				</div>
 				<div className="col-md-6">
 					<h4 className="pt-5">{data.title}</h4>
